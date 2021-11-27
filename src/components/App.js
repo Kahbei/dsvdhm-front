e982ac25f70a242632c274197afa95b55b1b61b6
Kahbei <mengus.gary@gmail.com>
2021-11-27 15:10:27 +0100

    Change component location in 2 distinct folder

--- src/components/App.js
+++ src/components/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import { Route, Routes } from "react-router";
 import { Link } from "react-router-dom";
 
-import DataManager from "./DataManager/DataManager";
-import GameTitle from "./GameTitle/GameTitle";
-import ElementsDetails from "./ElementsDetails/ElementsDetails";
-import DeleteData from "./DeleteData/DeleteData";
-import CreateData from "./CreateData/CreateData";
-import ModificationData from "./ModificationData/ModificationData";
+import DataManager from "./EntityManager/DataManager/DataManager";
+import GameTitle from "./GameManager/GameTitle/GameTitle";
+import ElementsDetails from "./EntityManager/ElementsDetails/ElementsDetails";
+import DeleteData from "./EntityManager/DeleteData/DeleteData";
+import CreateData from "./EntityManager/CreateData/CreateData";
+import ModificationData from "./EntityManager/ModificationData/ModificationData";
 
 function App() {
     return (
