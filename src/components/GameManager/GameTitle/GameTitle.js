251991107e31a8402a784996c5d9d5d0816e6173
Smoltyme <barbier.valentin@hotmail.com>
2021-12-05 17:57:12 +0100

    Page d'accueil

--- src/components/GameManager/GameTitle/GameTitle.js
+++ src/components/GameManager/GameTitle/GameTitle.js
@@ -1,11 +1,24 @@
 import "./GameTitle.css";
 
 function GameTitle(props) {
-    return (
-        <>
-            <p>GAME TITLE</p>
-        </>
-    );
+  return (
+    <div class="gitadd">
+      <h1 class="Titre">Die Suche von die Hoff Mann</h1>
+      <img
+        id="templier"
+        src="https://www.cdiscount.com/pdt2/8/7/4/1/550x550/his2009948975874/rw/sticker-templier-en-priere.jpg"
+        alt="Templier qui prie"
+      />
+      <div class="bouton">
+        <a href="#" id="BoutonStart">
+          Start
+        </a>
+        <a href="#" id="BontonRuledesignertext">
+          Rule designer
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default GameTitle;
